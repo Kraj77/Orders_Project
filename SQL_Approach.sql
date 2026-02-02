7ebc1686de2db8c22c07c209cf2adb851472822e
Kraj77 <104292141+Kraj77@users.noreply.github.com>
2026-02-02 15:58:50 +0530

    Add files via upload

---
+++ SQL_Approach.sql
@@ -0,0 +1,21 @@
+-- SQL Approach
+
+SELECT
+	c.customer_id,
+	age,
+	item_name,
+	CAST(SUM(COALESCE(quantity, 0)) AS INTEGER) AS quantity
+FROM
+	Customer c
+INNER JOIN Sales s
+	ON c.customer_id = s.c.customer_id
+INNER JOIN 
+	Orders o
+ON s.sales_id = o.sales_id
+INNER JOIN
+	Items  i
+ON o.item_id = i.item_id
+	WHERE age BETWEEN 18 AND 35
+GROUP BY c.customer_id,age,item_name
+	HAVING SUM(COALESCE(quantity, 0)) > 0
+ORDER BY c.customer_id,item_name;
